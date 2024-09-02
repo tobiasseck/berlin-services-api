84447a9b536ebaf574e42f807be5105dfe5c3e9a
Tobias Seck <tobias@seck-web.de>
2024-09-02 10:06:15 +0200

    scraper fix for Services A-Z site

--- Scraper Build/scraper_v1.py
+++ Scraper Build/scraper_v1.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from sqlmodel import SQLModel, Field, create_engine, Session, select
+from tqdm import tqdm
 
 class Service(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
@@ -10,9 +11,15 @@ class Service(SQLModel, table=True):
 def create_db_and_tables(engine):
     SQLModel.metadata.create_all(engine)
 
-def insert_data(session, service_id, service_name, service_link):
-    service = Service(id=service_id, service_name=service_name, link=service_link)
-    session.add(service)
+def upsert_data(session, service_id, service_name, service_link):
+    existing_service = session.get(Service, service_id)
+    if existing_service:
+        existing_service.service_name = service_name
+        existing_service.link = service_link
+        session.add(existing_service)
+    else:
+        new_service = Service(id=service_id, service_name=service_name, link=service_link)
+        session.add(new_service)
     session.commit()
 
 def scrape_services():
@@ -24,14 +31,28 @@ def scrape_services():
     create_db_and_tables(engine)
     
     with Session(engine) as session:
-        for letter_section in soup.find_all('div', class_='azlist-letter'):
-            for service in letter_section.find_next('ul').find_all('li'):
+        letter_sections = soup.find_all('div', class_='azlist-letter')
+        
+        for letter_section in tqdm(letter_sections, desc="Scraping Sections"):
+            services = letter_section.find_next('ul').find_all('li')
+            
+            for service in tqdm(services, desc=f"Processing {letter_section.find('h2').text}", leave=False):
                 service_link = service.find('a')['href']
                 service_name = service.find('a').text.strip()
                 service_id = int(service_link.split('/')[-2])
 
-                insert_data(session, service_id, service_name, service_link)
+                upsert_data(session, service_id, service_name, service_link)
 
+def fetch_all_services():
+    engine = create_engine('sqlite:///services.db')
+    
+    with Session(engine) as session:
+        services = session.exec(select(Service)).all()
+        for service in services:
+            print(service)
+              
 if __name__ == "__main__":
     scrape_services()
     print("Data has been successfully scraped and stored in the database.")
+    # print("\n")
+    # fetch_all_services()  
