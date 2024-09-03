dfe931eea8f73eac30acb7653509b62c31f8ee11
Tobias Seck <tobias@seck-web.de>
2024-09-03 09:21:26 +0200

    adjust db read to show new tables

--- Scraper Build/read_db.py
+++ Scraper Build/read_db.py
@@ -1,25 +1,38 @@
 import pandas as pd
 from sqlmodel import create_engine, Session, select
-from scraper_v2 import Service, ServiceDetail, Standorte
+from scraper_v4 import Service, ServiceDetail, Standorte, StandorteServices
 
 def display_services():
     engine = create_engine('sqlite:///services.db')
     
     with Session(engine) as session:
+        # Fetch all services
         services = session.exec(select(Service)).all()
-        service_details = session.exec(select(ServiceDetail)).all()
-        standorte = session.exec(select(Standorte)).all()
-        
         services_df = pd.DataFrame([service.dict() for service in services])
-        service_details_df = pd.DataFrame([detail.dict() for detail in service_details])
-        standorte_df = pd.DataFrame([standort.dict() for standort in standorte])
         
+        # Fetch all service details
+        service_details = session.exec(select(ServiceDetail)).all()
+        service_details_df = pd.DataFrame([detail.dict() for detail in service_details])
+        
+        # Fetch all standorte
+        standorte = session.exec(select(Standorte)).all()
+        standorte_df = pd.DataFrame([standort.dict() for standort in standorte])
+
+        # Fetch the relationships between standorte and services
+        standorte_services = session.exec(select(StandorteServices)).all()
+        standorte_services_df = pd.DataFrame([{
+            'standort_id': rel.standort_id,
+            'service_id': rel.service_id
+        } for rel in standorte_services])
+
         print("Services:")
         print(services_df)
         print("\nService Details:")
         print(service_details_df)
         print("\nStandorte:")
         print(standorte_df)
+        print("\nStandorte-Services Relationships:")
+        print(standorte_services_df)
 
 if __name__ == "__main__":
     display_services()
